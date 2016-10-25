6f4b8cd8538ebe49690a62f34eec62c58fa7be65
mbMosman <mbmosman@users.noreply.github.com>
2016-10-25 06:34:49 -0500

    Updates mod 3

--- module-info.js
+++ module-info.js
@@ -1,7 +1,8 @@
 CCIS1505 - Fund. of Programming = {
 modules: [
   { course: "CCIS1505 - Fund. of Programming", title: "Getting Started ", moduleUrl: "http://htc-ccis1505.github.io/fall-2016/modules/intro", sort_order: 1, description: "This module provides an introduction the tools used for programming." },
-  { course: "CCIS1505 - Fund. of Programming", title: "Ruby Basics", moduleUrl: "http://htc-ccis1505.github.io/fall-2016/modules/ruby-basics", sort_order: 2, description: "This module provides an introduction to programming in Ruby." }
+  { course: "CCIS1505 - Fund. of Programming", title: "Ruby Basics", moduleUrl: "http://htc-ccis1505.github.io/fall-2016/modules/ruby-basics", sort_order: 2, description: "This module provides an introduction to programming in Ruby." },
+  { course: "CCIS1505 - Fund. of Programming", title: "Ruby Methods & Program Flow", moduleUrl: "http://htc-ccis1505.github.io/fall-2016/modules/ruby-methods-flow", sort_order: 2, description: "This module continues introducing basic programming in Ruby with conditional statements, iteration, arrays, and custom methods." }
 ],
  prerequisites: [
 ]
