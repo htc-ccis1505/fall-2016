02eed52376b914ba1558a739d56341fd4f3a0431
mbMosman <mbmosman@users.noreply.github.com>
2016-11-01 07:22:24 -0500

    added next module

--- module-info.js
+++ module-info.js
@@ -2,7 +2,8 @@ CCIS1505 - Fund. of Programming = {
 modules: [
   { course: "CCIS1505 - Fund. of Programming", title: "Getting Started ", moduleUrl: "http://htc-ccis1505.github.io/fall-2016/modules/intro", sort_order: 1, description: "This module provides an introduction the tools used for programming." },
   { course: "CCIS1505 - Fund. of Programming", title: "Ruby Basics", moduleUrl: "http://htc-ccis1505.github.io/fall-2016/modules/ruby-basics", sort_order: 2, description: "This module provides an introduction to programming in Ruby." },
-  { course: "CCIS1505 - Fund. of Programming", title: "Ruby Methods & Program Flow", moduleUrl: "http://htc-ccis1505.github.io/fall-2016/modules/ruby-methods-flow", sort_order: 2, description: "This module continues introducing basic programming in Ruby with conditional statements, iteration, arrays, and custom methods." }
+  { course: "CCIS1505 - Fund. of Programming", title: "Ruby Methods & Program Flow", moduleUrl: "http://htc-ccis1505.github.io/fall-2016/modules/ruby-methods-flow", sort_order: 2, description: "This module continues introducing basic programming in Ruby with conditional statements, iteration, arrays, and custom methods." },
+  { course: "CCIS1505 - Fund. of Programming", title: "Ruby More Strings & Iterators", moduleUrl: "http://htc-ccis1505.github.io/fall-2016/modules/ruby-more", sort_order: 3, description: "This module builds and expands on the Ruby programming code we've learned so far." }
 ],
  prerequisites: [
 ]
